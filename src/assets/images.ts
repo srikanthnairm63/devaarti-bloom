44860624fb672102966dbdd3d5ad2becbf8d82b0
srikanthnairm63 <srikanthm63@gmail.com>
2026-01-23 13:37:45 +0530

    Create images.ts

---
+++ src/assets/images.ts
@@ -0,0 +1,9 @@
+// Brand
+export { default as brandLogo } from "./devaarti-jasmine.jpeg";
+
+// Products
+export { default as productRose } from "./product-rose.jpeg";
+export { default as productJasmine } from "./product-jasmine.jpeg";
+export { default as productLavender } from "./product-lavender.jpeg";
+export { default as productLogo } from "./product-logo.jpeg";
+export { default as productExtra } from "./product-extra.jpeg";
